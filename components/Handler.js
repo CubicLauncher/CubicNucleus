da0d8d3ddc971f107de4ef7d57d92ff9ab209dcd
SantiagolxxGG <149891004+SantiagolxxGG@users.noreply.github.com>
2024-11-15 19:13:47 -0300

    Delete components directory

--- components/Handler.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  Downloader: require('./downloader'), // Exporting the Downloader class from the downloader module
-  Launcher: require('./launcher') // Exporting the Launcher class from the launcher module
-};
